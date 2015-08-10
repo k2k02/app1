e21524e7ff585b423abcfdcc489e10525fa1e5c7
wkk91193 <wkk91193@yahoo.com>
2015-08-10 18:20:54 +0530

    first commit

--- public/javascripts/processJson.js
+++ public/javascripts/processJson.js
@@ -5,11 +5,23 @@
  */
 
 
-var getJson = angular.module('getJson', ['ngRoute']).controller('controller1', function ($scope, $http) {
-    var url = "../../routes/fields.js";
-    console.log(url);
-    $http.get(url).success(function (data) {
-        $scope.posts = data.data;
-    });
+//var getJson = angular.module('getJson', ['ngRoute']).controller('controller1', function ($log) {
+//    
+//    $log.debug('hello');
+//    
+////    var url = '/fields/fieldlist';
+////    console.log(url);
+////    $http.get(url).success(function (data,$log) {
+////        //$scope.posts = data;
+////       $log.debug('hello');
+////       
+////    });
+//
+//
+//});
 
-});
+angular.module('app', [])
+
+.controller('MainCtrl', ['$log', function($log){
+    $log.debug('Hello Debug!');
+}]);
